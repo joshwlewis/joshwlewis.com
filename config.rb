8bc0e2a31f8f47663d93d1749dfd59dd9b46eb18
Josh Lewis <josh.w.lewis@gmail.com>
2013-02-03 10:56:38 -0600

    Recreate site from basic html5 boilerplate

--- config.rb
+++ config.rb
@@ -1,15 +1,9 @@
-#Markdown
-set :markdown_engine, :redcarpet
-
-#Livereload
-activate :livereload
-
-### 
+###
 # Compass
 ###
 
 # Susy grids in Compass
-# First: gem install compass-susy-plugin
+# First: gem install susy
 # require 'susy'
 
 # Change Compass configuration
@@ -22,13 +16,13 @@ activate :livereload
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
@@ -53,34 +47,31 @@ activate :livereload
 #   end
 # end
 
-set :css_dir, 'stylesheets'
+set :css_dir, 'css'
 
-set :js_dir, 'javascripts'
+set :js_dir, 'js'
 
-set :images_dir, 'images'
+set :images_dir, 'img'
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  activate :minify_css
-  
+  # activate :minify_css
+
   # Minify Javascript on build
-  activate :minify_javascript
-  
-  # Create favicon/touch icon set from source/favicon_base.png
-  activate :favicon_maker
-  
+  # activate :minify_javascript
+
   # Enable cache buster
   # activate :cache_buster
-  
+
   # Use relative URLs
   # activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
